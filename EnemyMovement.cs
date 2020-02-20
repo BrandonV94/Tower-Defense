cc9f06bf98218d427be34470429eb0ff412dbab7
Brandon <bvasquez1994@gmail.com>
2020-02-20 15:04:13 -0500

    Add files via upload

---
+++ EnemyMovement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    IEnumerator FollowPath()
+    {
+        print("Starting Patrol.");
+        foreach (Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(1);
+        }
+        print("Ending Patrol");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    } 
+}
---
+++ PathFinder.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+    [SerializeField] Waypoint startWaypoint = null, endWaypoint = null;
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Queue<Waypoint> queue = new Queue<Waypoint>();
+    [SerializeField] bool isRunning = true;
+
+    Vector2Int[] directions =
+    {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
+
+    void Start()
+    {
+        LoadBlocks();
+        ColorStartAndEnd();
+        PathFind();
+    }
+
+    void PathFind()
+    {
+        queue.Enqueue(startWaypoint);
+
+        while (queue.Count > 0 && isRunning)
+        {
+            var searchCenter = queue.Dequeue();
+            searchCenter.isExplored = true;
+            print("Searching from " + searchCenter);
+            StopIfEndFound(searchCenter);
+            ExploreNeighbors(searchCenter);
+
+        }
+        print("Finished pathfinding?");
+    }
+
+    private void StopIfEndFound(Waypoint searchCenter)
+    {
+        if (searchCenter == endWaypoint)
+        {
+            print("Stopped searching for path.");
+            isRunning = false;
+        }
+    }
+
+    private void ExploreNeighbors(Waypoint from)
+    {
+        if(!isRunning) { return; }
+
+        foreach (Vector2Int direction in directions)
+        {
+            Vector2Int neighborCoordinates = startWaypoint.GetGridPos() + direction;
+            try
+            {
+                QueueNewNeighbors(neighborCoordinates);
+            }
+            catch
+            {
+                //do nothing
+            }
+        }
+    }
+
+    private void QueueNewNeighbors(Vector2Int neighborCoordinates)
+    {
+        Waypoint neighbor = grid[neighborCoordinates];
+        if(neighbor.isExplored || queue.Contains(neighbor))
+        {
+            // do nothing 
+        }
+        else
+        {
+            neighbor.SetTopColor(Color.blue);
+            queue.Enqueue(neighbor);
+            print("Queueing " + neighbor);
+        }
+    }
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach(Waypoint waypoint in waypoints)
+        {
+            var gridPos = waypoint.GetGridPos();
+            if(grid.ContainsKey(gridPos))
+            {
+                Debug.LogWarning("Skipping Overlapping block " + waypoint);
+            }
+            else 
+            {
+                grid.Add(waypoint.GetGridPos(), waypoint);
+            }
+        }
+        print("Loaded" + grid.Count + " blocks");
+    }
+
+    private void ColorStartAndEnd()
+    {
+        startWaypoint.SetTopColor(Color.green);
+        endWaypoint.SetTopColor(Color.red);
+    }
+}
